8c2820a4f2e84c453e079f63b9eb7fb7ca53a962
astro1345 <aleverdial@gmail.com>
2025-08-02 16:27:09 -0600

    loginy ocsitas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{
+    path: '',
+    loadComponent: () => import('./componentes/login/login').then(m => m.Login)
+},
+{
+    path: 'inicio',
+    loadComponent: () => import('./componentes/inicio/inicio').then(m => m.Inicio)
+},
+{
+    path: 'registro',
+    loadComponent: () => import('./componentes/registro/registro').then(m => m.Registro)
+},
+
+];
