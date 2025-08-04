8be734f6eb60f139f12bacfc4eb7c1a92fc4e340
astro1345 <aleverdial@gmail.com>
2025-08-04 13:46:52 -0600

    antes de cagar

---
+++ src/app/paginas/miperfil/miperfil.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-miperfil',
+  imports: [],
+  templateUrl: './miperfil.html',
+  styleUrl: './miperfil.scss'
+})
+export class Miperfil {
+
+}
