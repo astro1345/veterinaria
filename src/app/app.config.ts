468b4f78cf3777bd51f8cf76fb4c4baa9b96d851
Francis1316 <francisposadas2003@gmail.com>
2025-08-10 01:24:38 -0600

    MascotaForm

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,30 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: "AIzaSyCFfCZR1xxBbKmei092kLY66Th4D8Wf9ng",
+        authDomain: "veterinariahn-307b3.firebaseapp.com",
+        projectId: "veterinariahn-307b3",
+        storageBucket: "veterinariahn-307b3.firebasestorage.app",
+        messagingSenderId: "895635522707",
+        appId: "1:895635522707:web:064d078ff3f4e7daf43966",
+        measurementId: "G-8MK0WE4VHH",
+      })
+    ),
+    provideFirestore(() => getFirestore()),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
   ]
 };
--- src/app/paginas/inicio/inicio.ts
+++ src/app/paginas/inicio/inicio.ts
@@ -1,14 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Navbar } from "../../componentes/navbar/navbar";
 import { Footer } from "../../componentes/footer/footer";
 import { RouterLink, RouterOutlet } from '@angular/router';
-
+import { Mascotas } from '../../servicios/mascotas';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { Mascotaform } from '../../componentes/mascotaform/mascotaform';  
 @Component({
   selector: 'app-inicio',
-  imports: [Navbar, Footer,RouterLink, RouterOutlet],
+  standalone: true,
+  imports: [Navbar, Footer, RouterLink, RouterOutlet, JsonPipe, CommonModule, Mascotaform],
   templateUrl: './inicio.html',
   styleUrl: './inicio.scss'
 })
 export class Inicio {
+  mascotaService = inject(Mascotas);
+  mascotas: any[] = [];
+  mostrarModal = false;
 
+  constructor() {
+    this.getMascotas();
+  }
+
+  abrirModal() {
+    this.mostrarModal = true;
+  }
+
+  cerrarModal() {
+    this.mostrarModal = false;
+  }
+
+  agregarMascota(mascota: any) {
+    this.mascotaService.addMascota(mascota).then(() => {
+      this.getMascotas();
+    });
+  }
+
+  eliminarMascota(uuid: string) {
+    if (confirm('¿Estás seguro de que quieres eliminar esta mascota?')) {
+      this.mascotaService.deleteMascota(uuid).then(() => {
+        this.getMascotas();
+      });
+    }
+  }
+
+  getMascotas() {
+    this.mascotaService.getMascotas().subscribe((data: any) => {
+      this.mascotas = data;
+    });
+  }
 }
