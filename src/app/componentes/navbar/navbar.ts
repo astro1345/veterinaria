8cfa9a00863e136b39922e5ea0250208d5b18a8b
astro1345 <117995337+astro1345@users.noreply.github.com>
2025-08-11 20:21:41 -0600

    Crud seguimiento medico (#13)
    * CRUD-Vacunas
    
    * deploy & confirmacion
    
    * colores y validacion

--- src/app/componentes/navbar/navbar.ts
+++ src/app/componentes/navbar/navbar.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Mascotas } from '../../servicios/mascotas';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
+  mascotaService = inject(Mascotas);
+  mascotas: any[] = [];
 
+  constructor() {
+    this.getMascotas();
+  }
+
+  getMascotas() {
+    this.mascotaService.getMascotas().subscribe((data: any) => {
+      this.mascotas = data;
+    });
+  }
 }
