1c1bac5e5f940ac04d1e3a8a10c11e1daab3c435
Francis1316 <francisposadas2003@gmail.com>
2025-09-05 23:08:09 -0600

    historial

--- src/app/paginas/vermascota/vermascota.ts
+++ src/app/paginas/vermascota/vermascota.ts
@@ -6,12 +6,14 @@ import { Users } from '../../servicios/users';
 import { Session } from '../../servicios/session';
 import { Navbar } from "../../componentes/navbar/navbar";
 import { Notificacionesysolitud } from '../../servicios/notificacionesysolitud';
-import { DatePipe } from '@angular/common';
-
+import { CommonModule, DatePipe } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { VacunaService } from '../../servicios/vacuna';
 
 @Component({
   selector: 'app-vermascota',
-  imports: [Footer, Navbar,DatePipe],
+  standalone: true,
+  imports: [Footer, Navbar, DatePipe, CommonModule, ReactiveFormsModule],
   templateUrl: './vermascota.html',
   styleUrls: ['./vermascota.scss']
 })
@@ -21,7 +23,7 @@ export class Vermascota implements OnInit {
   userid: string | null = this.sesionService.getUid();
   notificacion: any = {};
   solicitante: any = {};
-   vacunas: any[] = [];
+  vacunas: any[] = [];
   mascota: any = {};
   duenio: any = {};
   logeado: boolean = false;
@@ -29,11 +31,29 @@ export class Vermascota implements OnInit {
   router = inject(Router);
   userService = inject(Users);
   mensaje: string = '';
+
+  // 🔹 Nuevo
+  vacunaForm: FormGroup;
+  mostrarModalVacuna = false;
+
   constructor(
     private route: ActivatedRoute,
     private mascotaService: Mascotas,
-
-  ) { }
+    private fb: FormBuilder,
+    private vacunaService: VacunaService
+  ) { 
+    // Inicializamos el formulario
+    this.vacunaForm = this.fb.group({
+      id: [null],
+      nombre: ['', Validators.required],
+      tipo: ['', Validators.required],
+      fecha: ['', Validators.required],
+      cantidad: ['', Validators.required],
+      periodo: ['', Validators.required],
+      notas: [''],
+      agregadoPor: [''] 
+    });
+  }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
@@ -41,55 +61,47 @@ export class Vermascota implements OnInit {
       this.cargarMascota(id);
       this.estaLogeado();
       this.esVeterinario(id);
+      this.cargarVacunas(id);
     });
-
   }
 
   async cargarMascota(id: string) {
     try {
       this.mascota = await this.mascotaService.getMascotaById(id);
-    
-    this.duenio = await this.userService.getUserporid(this.mascota.idduenio);
- 
-if(this.userid !== null) {
-this.solicitante = await this.userService.getUserporid(this.userid);
-  
-}
+      this.duenio = await this.userService.getUserporid(this.mascota.idduenio);
 
+      if (this.userid !== null) {
+        this.solicitante = await this.userService.getUserporid(this.userid);
+      }
     } catch (error) {
       console.error('Error cargando mascota:', error);
     }
   }
 
-
- estaLogeado() {
-  this.logeado = this.sesionService.isLoggedIn();
-
-  
-  
- 
-}
-
-
-esVeterinario(idusuario: string ) {
-  if (idusuario) {
-    this.mascotaService.esvet(idusuario).then(esvet => {
-      this.esvet = esvet;
-      console.log("Es veterinario:", esvet);
-    }).catch(err => console.error(err));
-  } else {
-    console.error("Error: userid is null");
+  cargarVacunas(id: string) {
+    this.vacunaService.getVacunas(id).subscribe(data => {
+      this.vacunas = data;
+    });
   }
-}
-  cerrarSesion() {
 
+  estaLogeado() {
+    this.logeado = this.sesionService.isLoggedIn();
+  }
+
+  esVeterinario(idusuario: string ) {
+    if (idusuario) {
+      this.mascotaService.esvet(idusuario).then(esvet => {
+        this.esvet = esvet;
+        console.log("Es veterinario:", esvet);
+      }).catch(err => console.error(err));
+    }
+  }
+
+  cerrarSesion() {
     this.router.navigate(['']);
   }
 
-
   async solicitar() {
-
-
     if (this.solicitante.nombre) {
       this.mensaje = `${this.solicitante.nombre} con email ${this.solicitante.email} quiere ser veterinario de ${this.mascota.nombre}`;
     } else {
@@ -108,7 +120,32 @@ esVeterinario(idusuario: string ) {
         alert('Solicitud enviada');
       })
       .catch(err => alert('Error al enviar solicitud ' + err));
-
   }
 
-} 
+  // 🔹 Métodos para vacunas SOLO agregar
+  abrirFormularioVacuna() {
+    this.vacunaForm.reset();
+    this.mostrarModalVacuna = true;
+    document.body.style.overflow = 'hidden';
+  }
+
+  cerrarModalVacuna() {
+    this.mostrarModalVacuna = false;
+    document.body.style.overflow = '';
+  }
+
+  guardarVacuna() {
+    if (this.vacunaForm.invalid) {
+      this.vacunaForm.markAllAsTouched();
+      return;
+    }
+
+    const vacunaData = this.vacunaForm.value;
+    this.vacunaService.addVacuna(this.mascota.idmascota, vacunaData)
+      .then(() => {
+        this.cerrarModalVacuna();
+      })
+      .catch(err => alert('Error agregando vacuna: ' + err));
+  }
+
+}
